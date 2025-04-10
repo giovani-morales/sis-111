51702976fed5c13fca5e69de4db6105e3c475e7f
giovani-morales <giovani.morales@ucb.edu.bo>
2025-04-10 09:54:18 -0400

    Clase de programacion
    calculo de suma de palabras, resta y negación, multiplicación para obtener un área y por ultimo obtener el valor de una división el cociente

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,39 @@
+
+#CREAR UN VAR
+saludo = "Hola mundo ! ! !"
+print(saludo)
+
+
+#suma de numeros
+num1 = 10
+num2 = 20
+suma = num1 + num2
+print(suma)
+
+#suma de palabras -> concatenar palabras
+palabra1 = "hola"
+palabra2 = "mundo"
+suma_palabras = palabra1 + " " + palabra2
+print(suma_palabras)
+
+
+# Resta
+y = 10 - 4
+print("Resultado de la resta:", y)  # Resultado: 6
+
+
+# Negar un valor
+z = -5
+print("Valor negativo:", z)  # Resultado: -5
+
+
+# Multiplicación
+base = 4
+altura = 3
+area = base * altura
+print("Área:", area)  # Resultado: 12
+
+
+# División
+cociente = 15 / 2
+print("Cociente:", cociente)  # Resultado: 7.5
